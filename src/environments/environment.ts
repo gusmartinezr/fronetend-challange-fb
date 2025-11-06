5757fdb8e11da02476d7e7b7c96a5eaac631d5d9
Gustavo Martinez Rollano <54968322+gusmartinezr@users.noreply.github.com>
2025-11-06 13:48:53 -0400

    feature: new url 2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://api-j6bgvnr3ua-uc.a.run.app/__health/api'
+  apiUrl: 'https://api-j6bgvnr3ua-uc.a.run.app/api'
   // apiUrl: 'https://3.95.177.182/api'
 };
